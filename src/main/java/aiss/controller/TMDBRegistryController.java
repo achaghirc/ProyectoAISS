3b8fa283712e6727de5c75df82f738ea2473dcb7
achaghirc00 <amine-10@hotmail.es>
2019-04-29 20:42:52 +0200

    EMI

--- src/main/java/aiss/controller/TMDBRegistryController.java
+++ src/main/java/aiss/controller/TMDBRegistryController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import aiss.Movie.Movie;
 import aiss.model.resources.MovieResources;
+import aiss.model.tmdb.Sesion;
 import aiss.model.tmdb.Session;
 import aiss.model.tmdb.TMDBRegistry;
 
@@ -29,13 +30,17 @@ public class TMDBRegistryController extends HttpServlet{
 		MovieResources mvr = new MovieResources();
 		TMDBRegistry t = mvr.getToken();
 		String token  = t.getRequestToken();
+		Sesion s = new Sesion();
+		String sesionId=s.getSessionId();
 		
 		
 		if(token!=null && ""!=token) {	
 			rd = request.getRequestDispatcher("/inicio.jsp");
 			request.setAttribute("token",token);
+			request.setAttribute("sesionId", sesionId);
+			log.log(Level.FINE, "El token es= "+token );
+			log.log(Level.FINE, "El id de sesion es= "+sesionId);
 			
-			log.log(Level.FINE, "La peli con id="+token,"ha sido mostrada" );
 		}else {
 			request.setAttribute("movie", "La pelicula no se ha podido mostrar");
 			log.log(Level.FINE, "La peli con id="+token,"no ha sido mostrada" );
