10e120a786279f2a0d6323dbdba15f72059b99f8
GermanTheWanker <44214733+GermanTheWanker@users.noreply.github.com>
2019-05-14 17:21:35 +0200

    Pruebas

--- src/main/java/aiss/controller/TMDBRegistryController.java
+++ src/main/java/aiss/controller/TMDBRegistryController.java
@@ -3,16 +3,12 @@ package aiss.controller;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import aiss.Movie.Movie;
 import aiss.model.resources.MovieResources;
-import aiss.model.tmdb.Session;
 import aiss.model.tmdb.TMDBRegistry;
 
 public class TMDBRegistryController extends HttpServlet{
